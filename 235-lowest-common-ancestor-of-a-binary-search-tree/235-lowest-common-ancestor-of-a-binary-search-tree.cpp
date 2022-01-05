4689c40dd1c68ad2b170d192a10f1591d04e132c
manvendraaa <73592861+manvendraaa@users.noreply.github.com>
2022-01-05 17:11:54 +0530

    Time: 43 ms (14.02%), Space: 23.3 MB (10.98%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,15 +11,9 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root)return root;
-        TreeNode* a = p->val<q->val?p:q;
-        TreeNode* b = p->val>q->val?p:q;
-        
-        if(root->val >= a->val && root->val <= b->val)return root;
-        
-        if(root->val < a->val)return lowestCommonAncestor(root->right,p,q);
-        if(root->val > b->val)return lowestCommonAncestor(root->left,p,q);
-        
+    // if dono se bada hai to left me dhoondo dono se chota to right warna root is the ans;
+        if(root->val > p->val && root->val > q->val)return lowestCommonAncestor(root->left,p,q);
+        if(root->val < p->val && root->val < q->val)return lowestCommonAncestor(root->right,p,q);
         return root;
     }
 };
