4bc93c8466540980b3eb6b7bb754487520fa52ec
manvendraaa <73592861+manvendraaa@users.noreply.github.com>
2022-01-08 18:36:05 +0530

    Time: 8 ms (79.45%), Space: 13.8 MB (91.15%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -5,12 +5,12 @@ class Solution {
 public:
     
     void dfs(vector<vector<int>>& image, int i, int j, int newColor,int color){
-        if(i < 0 || i>= image.size() || j<0 || j>=image[0].size() || image[i][j] != color)return;
         image[i][j] = newColor;
         
         for(int c = 0 ;c < 4; c++){
             int k = i + x[c];
             int p = j + y[c];
+              if(k < 0 || k>= image.size() || p<0 || p>=image[0].size() || image[k][p] != color)continue;
             dfs(image,k,p,newColor,color);
         }
         
