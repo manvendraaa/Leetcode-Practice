a7bd792da9a373c5e686f266f7ad2ca763928527
manvendraaa <73592861+manvendraaa@users.noreply.github.com>
2022-01-22 22:52:34 +0530

    Time: 62 ms (48.78%), Space: 19.1 MB (9.29%) - LeetHub

---
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        // number of set bits in a^b
+        int n = nums.size();
+        int ans = 0;
+        
+        // for all the nums count the total 1s at a idx and total 0s finally you can choose any one and zero to make diff bit and so the ans will be for that index 1s*0s 
+        vector<pair<int,int>> res;
+        int count;
+        for(int i = 0;i<32;i++){
+            count = 0;
+            for(int j = 0;j<n;j++){
+                if(nums[j] &(1<<i)){
+                    count++;
+                }
+                
+            }
+            res.push_back({count,n-count});
+        }
+        
+        for(auto i: res){
+            ans += i.first*i.second;
+        }
+        
+        
+        return ans;
+        
+        
+        
+        
+    }
+};
