113a8b5c2ad252b7ab620ea11eefad23569391dd
manvendraaa <73592861+manvendraaa@users.noreply.github.com>
2022-01-22 22:53:20 +0530

    Time: 84 ms (25.51%), Space: 19 MB (92.24%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -6,7 +6,7 @@ public:
         int ans = 0;
         
         // for all the nums count the total 1s at a idx and total 0s finally you can choose any one and zero to make diff bit and so the ans will be for that index 1s*0s 
-        vector<pair<int,int>> res;
+        // vector<pair<int,int>> res;/
         int count;
         for(int i = 0;i<32;i++){
             count = 0;
@@ -16,12 +16,12 @@ public:
                 }
                 
             }
-            res.push_back({count,n-count});
+            ans+= count*(n-count);
         }
         
-        for(auto i: res){
-            ans += i.first*i.second;
-        }
+//         for(auto i: res){
+//             ans += i.first*i.second;
+//         }
         
         
         return ans;
